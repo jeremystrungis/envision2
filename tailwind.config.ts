f4df4ded147626909c0c33dd20a5cdfd2cf1be5c
JeremyStrungis <jeremystrungis@gmail.com>
2025-07-30 19:10:29 +0000

    nope, still the same. specifically, I am seeing the "Develop UI Componen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,7 +97,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'slow-pulse': 'slow-pulse 8s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'slow-pulse': 'slow-pulse 20s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
